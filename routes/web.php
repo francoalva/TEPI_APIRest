2b6d536e0f18ddddea8250639fd198ac56d8f6b3
francoalva <francoalvaralv@gmail.com>
2022-07-16 21:16:17 -0400

    depurando cors

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// CRUD BODEGAS
-Route::get('/bodegas','App\Http\Controllers\BodegaController@index');
-Route::post('/bodegas','App\Http\Controllers\BodegaController@store');
-Route::put('/bodegas/{id}','App\Http\Controllers\BodegaController@update');
-Route::delete('/bodegas/{id}','App\Http\Controllers\BodegaController@destroy');
 
-//
-Route::get('/marcas','App\Http\Controllers\MarcaController@index');
-//
-Route::get('/modelos','App\Http\Controllers\ModeloController@index');
-Route::get('/modelos/marca/{id}','App\Http\Controllers\ModeloController@show');
-//
-Route::get('/dispositivos','App\Http\Controllers\DispositivoController@index');
-Route::get('/dispositivos/modelo/{id}','App\Http\Controllers\DispositivoController@showByModelo');
-Route::get('/dispositivos/marca/{id}','App\Http\Controllers\DispositivoController@showByMarca');
-Route::get('/dispositivos/bodega/{id}','App\Http\Controllers\DispositivoController@showByBodega');
+Route::group(['middleware' => ['cors']], function () {
+    //Rutas a las que se permitirá acceso
+    // CRUD BODEGAS
+    Route::get('/bodegas','App\Http\Controllers\BodegaController@index');
+    Route::post('/bodegas','App\Http\Controllers\BodegaController@store');
+    Route::put('/bodegas/{id}','App\Http\Controllers\BodegaController@update');
+    Route::delete('/bodegas/{id}','App\Http\Controllers\BodegaController@destroy');
 
-// Recurso especifico para los combobox
-Route::get('/dispositivos/{bodega}/{marca}/{modelo}','App\Http\Controllers\DispositivoController@filterBy');
+    //
+    Route::get('/marcas','App\Http\Controllers\MarcaController@index');
+    //
+    Route::get('/modelos','App\Http\Controllers\ModeloController@index');
+    Route::get('/modelos/marca/{id}','App\Http\Controllers\ModeloController@show');
+    //
+    Route::get('/dispositivos','App\Http\Controllers\DispositivoController@index');
+    Route::get('/dispositivos/modelo/{id}','App\Http\Controllers\DispositivoController@showByModelo');
+    Route::get('/dispositivos/marca/{id}','App\Http\Controllers\DispositivoController@showByMarca');
+    Route::get('/dispositivos/bodega/{id}','App\Http\Controllers\DispositivoController@showByBodega');
+
+    // Recurso especifico para los combobox
+    Route::get('/dispositivos/{bodega}/{marca}/{modelo}','App\Http\Controllers\DispositivoController@filterBy');
+});
+
 
 
 
